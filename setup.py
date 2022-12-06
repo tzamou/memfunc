bf2a49a35630bab83a91d063681cf11d63a5f3d3
tzamou <72702965+tzamou@users.noreply.github.com>
2022-12-06 23:17:00 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="pymf",#python membership function
+    version="0.0.1",
+    author="Guan-Lin Chen",
+    author_email="ken54787845@gmail.com",
+    description="It is used to quickly establish membership functions for subsequent fuzzy transformations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
