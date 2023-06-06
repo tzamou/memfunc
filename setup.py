058cae3fc9f17cded1ab0aaefdea04f7ccec7e7d
Kuan-Lin Chen <72702965+tzamou@users.noreply.github.com>
2023-06-06 21:25:11 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 setuptools.setup(
     name="pymf",#python membership function
     version="0.0.1",
-    author="Guan-Lin Chen",
+    author="Kuan-Lin Chen",
     author_email="ken54787845@gmail.com",
     description="It is used to quickly establish membership functions for subsequent fuzzy transformations.",
     long_description=long_description,
